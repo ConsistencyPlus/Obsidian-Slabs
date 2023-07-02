71a84accf070ccdfdc650e50c4d394cb89339cb3
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2023-07-01 21:31:18 -0500

    This is the mod of all time

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Literally just adds the Obsidian slab from Consistency+ as a separate block.
 
-IMS was the guy in charge of a lot of the actual code when making the initial block. Siuol took care of the separation after the fact and maintains this specific mod.
+IMS (Yes, Iris Shaders IMS), a member of the C+ team at the time was the guy in charge of alot of the actual code when making the inital block, Siuol took care of the separation after the fact and maintains this specific mod.
--- src/main/java/io/github/consistencyplus/obsidianslabs/ObsidianSlabs.java
+++ src/main/java/io/github/consistencyplus/obsidianslabs/ObsidianSlabs.java
@@ -1,14 +1,16 @@
 package io.github.consistencyplus.obsidianslabs;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static net.minecraft.block.piston.PistonBehavior.BLOCK;
 
@@ -16,9 +18,13 @@ import static net.minecraft.block.piston.PistonBehavior.BLOCK;
 public class ObsidianSlabs implements ModInitializer {
     public static final Identifier namespace = new Identifier("obsidian_slab", "obsidian_slab");
 
-    public static final Block OBSIDIAN_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.of(new Material(MapColor.BLACK, false, true, true, true, false, false, BLOCK)).requiresTool().strength(50.0F, 1200.0F)));
-    public static final Item OBSIDIAN_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(OBSIDIAN_SLAB, new Item.Settings().group(ItemGroup.REDSTONE)));
+    public static final Block OBSIDIAN_SLAB = Registry.register(Registries.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.copy(Blocks.OBSIDIAN)));
+    public static final Item OBSIDIAN_SLAB_ITEM = Registry.register(Registries.ITEM, namespace, new BlockItem(OBSIDIAN_SLAB, new Item.Settings()));
 
     @Override
-	public void onInitialize() {}
-}
+	public void onInitialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register((fabricItemGroupEntries)-> {
+            fabricItemGroupEntries.add(OBSIDIAN_SLAB);
+        });
+
+    }}
