874711520af1f7684eae0b012d946644c28d12bd
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2023-07-29 03:45:23 -0500

    "Carter is a bitch ass motherfucker he pushed my fucking Obsidian Slab"

--- src/main/java/io/github/consistencyplus/obsidianslabs/ObsidianSlabs.java
+++ src/main/java/io/github/consistencyplus/obsidianslabs/ObsidianSlabs.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.*;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -18,7 +19,7 @@ import static net.minecraft.block.piston.PistonBehavior.BLOCK;
 public class ObsidianSlabs implements ModInitializer {
     public static final Identifier namespace = new Identifier("obsidian_slab", "obsidian_slab");
 
-    public static final Block OBSIDIAN_SLAB = Registry.register(Registries.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.copy(Blocks.OBSIDIAN)));
+    public static final Block OBSIDIAN_SLAB = Registry.register(Registries.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.copy(Blocks.OBSIDIAN).pistonBehavior(BLOCK)));
     public static final Item OBSIDIAN_SLAB_ITEM = Registry.register(Registries.ITEM, namespace, new BlockItem(OBSIDIAN_SLAB, new Item.Settings()));
 
     @Override
